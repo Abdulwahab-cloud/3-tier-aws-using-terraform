f25d7781d52066a746530542950aa817d622571b
Abdulwahab Abdelkarim <abdlwahab.kariim@gmail.com>
2025-04-07 15:19:36 +0200

    userdata.sh

--- user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World , This is my first AWS project with Terraform</h1>" > /var/www/html/index.html
