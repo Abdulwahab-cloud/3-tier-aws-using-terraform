2bf0e49dca4c604e4846f32beac864cbee007f4e
Abdulwahab Abdelkarim <abdlwahab.kariim@gmail.com>
2025-04-07 15:20:03 +0200

    asg.tf

--- asg.tf
+++ asg.tf
@@ -4,7 +4,7 @@ resource "aws_launch_template" "web" {
   image_id               = var.ami_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.web.id]
-  user_data              = filebase64("${path.module}/user.sh")
+  user_data              = filebase64("${path.module}/userdata.sh")
   lifecycle {
     create_before_destroy = true
   }
